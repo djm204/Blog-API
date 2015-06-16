3c044cbf3b0ee5d7b0d457af55c821e876c35942
Desiratech Solutions <david@desiratech.com>
2015-06-16 00:32:40 -0500

    Finally got a db to work

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Make our db accessible to our router
-app.use(function(req,res,next){
+/*app.use(function(req,res,next){
     req.db = db;
     next();
-});
+});*/
 
 
 app.use('/', routes);
