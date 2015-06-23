251bab1dafa7b01c03da5bbb5a7ccc337c839074
David Mendez <desiratech@gmail.com>
2015-06-22 19:55:26 -0500

    View engine error fixed, .render requires view engine

--- server.js
+++ server.js
@@ -4,21 +4,13 @@ var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var path = require('path');
 
-// view engine setup
-
 
 
 
 
 var app = express();
 
-var server = app.listen(4500, function (){
-  
-  var host = server.address().address;
-  var port = server.address().port;
-  
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+var server = app.listen(4500);
 
 
 // uncomment after placing your favicon in /public
@@ -38,34 +30,6 @@ app.use(cookieParser());
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
 
 module.exports = app;
